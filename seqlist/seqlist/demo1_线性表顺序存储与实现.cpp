dad6206cdfbc9d367c483eb15dbaff6cdb041a6f
Tang4109 <2298964704@qq.com>
2019-07-30 15:48:05 +0800

    链表测试框架的搭建

---
+++ seqlist/seqlist/demo1_线性表顺序存储与实现.cpp
@@ -0,0 +1,74 @@
+/****************************************!
+*@brief  
+*@author ZhangYunjia
+*@date   2019/7/30/14:59
+
+****************************************/
+
+#include<iostream>
+#include "seqlist.h"
+
+using namespace std;
+
+struct Teacher 
+{
+	int age;
+	char name[64];
+
+};
+
+
+int main()
+{
+	int ret = 0;
+
+	//创建
+	SeqList* list = NULL;
+	list = SeqList_Create(10);
+	if (list==NULL)
+	{
+		ret = -1;
+		cout << "error:" << ret << endl;
+		return ret;
+	}
+
+	Teacher t1, t2, t3, t4, t5;
+
+	t1.age = 31;
+	t2.age = 32;
+	t3.age = 33;
+	t4.age = 34;
+	t5.age = 35;
+
+	//插入
+	ret = SeqList_Insert(list, (SeqListNode*)&t1, 0);
+	ret = SeqList_Insert(list, (SeqListNode*)&t2, 0);
+	ret = SeqList_Insert(list, (SeqListNode*)&t3, 0);
+	ret = SeqList_Insert(list, (SeqListNode*)&t4, 0);
+	ret = SeqList_Insert(list, (SeqListNode*)&t5, 0);
+
+	//遍历
+	for (int i=0;i< SeqList_Length(list);i++)
+	{
+		Teacher* tmp = (Teacher*)SeqList_Get(list, i);
+		if (tmp==NULL)
+		{
+			ret = -2;
+			return ret;
+		}
+		cout << tmp->age << endl;
+	}
+	//删除
+	while (SeqList_Length(list))
+	{
+		SeqList_Delete(list, 0);
+
+	}
+	
+
+
+	
+	cout<<"end.."<<endl;
+	system("pause");
+	return 0;
+}
---
+++ seqlist/seqlist/seqlist.cpp
@@ -0,0 +1,64 @@
+#include "seqlist.h"
+#include<iostream>
+
+//链表 创建
+SeqList* SeqList_Create(int capacity)
+{
+	return NULL;
+
+}
+
+//链表 销毁
+void SeqList_Destroy(SeqList* list)
+{
+	return;
+
+}
+
+////链表 清空
+void SeqList_Clear(SeqList* list)
+{
+	return;
+
+}
+
+//链表 长度
+int SeqList_Length(SeqList* list)
+{
+	return 0;
+
+}
+
+
+//链表 容量 
+int SeqList_Capacity(SeqList* list)
+{
+	return 0;
+
+}
+
+//链表 在某一个位置 插入元素
+int SeqList_Insert(SeqList* list, SeqListNode* node, int pos)
+{
+	return 0;
+
+}
+
+//获取某一个位置的链表结点
+SeqListNode* SeqList_Get(SeqList* list, int pos)
+{
+	return NULL;
+
+}
+
+//删除某一个位置的结点
+SeqListNode* SeqList_Delete(SeqList* list, int pos)
+{
+	return NULL;
+
+}
+
+
+
+
+
